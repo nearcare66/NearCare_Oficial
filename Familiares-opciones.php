6748b3e1ec0044dc3d747ed8d15deca2a7e88322
CesarBenitez2027 <cesar.benitez2027@adoc.superate.org>
2026-05-26 21:53:23 -0600

    Creacion Familiares-opciones

---
+++ Familiares-opciones.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>NearCare</title>
+    <link rel="stylesheet" href="./styles4.css">
+</head>
+<body>
+
+<header class="navbar">
+    
+<div class="circle-big"></div>
+<div class="circle-small"></div>
+<div class="circle-right"></div>
+<div class="circle-corner"></div>
+
+    <div class="left">
+        ☰
+        <span class="back">⟵</span>
+        <img src="img/Designer (16).png" class="logo" alt="NearCare">
+    </div>
+
+    <div class="right">
+        <div class="avatar"></div>
+        <span class="bienvenido">Bienvenido</span>
+    </div>
+</header>
+
+<div class="background-shapes">
+    <div class="circle big"></div>
+    <div class="circle small"></div>
+    <div class="circle tiny"></div>
+</div>
+
+<div class="container">
+    <button>Ingrese el código del paciente</button>
+    <button>Generar código de acceso</button>
+    <button>Condición del paciente</button>
+</div>
+
+</body>
+</html>
