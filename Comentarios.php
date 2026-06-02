901949f1b48e8a7710294ffab5522ecf4a91a090
CesarBenitez2027 <cesar.benitez2027@adoc.superate.org>
2026-06-02 17:16:00 -0600

    Creacion extraurdinaria de comentarios by César

---
+++ Comentarios.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>NearCare</title>
+  <link rel="stylesheet" href="Css/styles5.css">
+</head>
+<body>
+    <main>
+    <header class="navbar">
+
+    <a href="index.php" class="back-btn">←</a>
+
+    <div class="logo">
+        <img src="img/Designer (16).png" alt="">
+    </div>
+
+    <div class="profile">
+        <img src="https://cdn-icons-png.flaticon.com/512/4140/4140048.png">
+        <span class="welcome">Bienvenido</span>
+    </div>
+
+    </header>
+    </main>
+
+    <div class="bg-bubbles">
+    <div class="b1"></div>
+    <div class="b2"></div>
+    <div class="b3"></div>
+    <div class="b4"></div>
+
+    <!-- nuevas -->
+    <div class="b5"></div>
+    <div class="b6"></div>
+    <div class="b7"></div>
+    <div class="b8"></div>
+    <div class="b9"></div>
+    </div>
+    <div class="container">
+
+
+        <div class="info-bar">
+            NearCare agradecerá tu opinión sobre nuestros trabajo y que podemos implementar
+        </div>
+
+        
+        <div class="comment-box">
+            <textarea placeholder="Escribe tu comentario..."></textarea>
+        </div>
+
+
+        <button class="btn-enviar">Enviar</button>
+
+    </div>
+
+</body>
+</html>
