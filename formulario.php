744839a3ae599eeac1b6ab036ef40e8b6d2bd182
Gabriela Guzman <gabriela.guzman2027@adoc.superate.org>
2026-06-07 19:40:04 -0600

    Pagina de formulario ya esta terminada

--- formulario.php
+++ formulario.php
@@ -14,66 +14,69 @@
 </head>
 <body>
 
-<div class="contenedor">
-
-   
     <nav class="navbar">
         <div class="nav-left">
+            <span class="menu-icon">&#9776;</span> 
             <img src="img/Designer (16).png" alt="Logo NearCare" class="logo">
         </div>
 
         <div class="nav-right">
-            <img src="img/avatar.png" alt="Usuario" class="avatar">
+           
+            <img src="img/avatar_hombre.png" alt="Usuario" class="avatar">
             <div class="bienvenido">
                 <span>Bienvenido</span>
             </div>
         </div>
     </nav>
 
-    <div class="titulo">
-        Registrar paciente
+    <div class="contenedor">
+
+        <h1>Registrar Paciente</h1>
+
+
+        <form method="POST">
+
+            <label>ID de paciente *</label>
+            <input type="text" name="id_paciente" placeholder="Ingrese el ID">
+
+            <label>Nombre completo del paciente *</label>
+            <input type="text" name="nombre" placeholder="Ingrese el nombre">
+
+            <label>Edad actual *</label>
+            <input type="text" name="edad" placeholder="Ingrese la edad">
+
+            <label>Género *</label>
+            <select name="genero">
+                <option>Seleccione una opción</option>
+                <option>Masculino</option>
+                <option>Femenino</option>
+            </select>
+
+            <label>Condición del paciente *</label>
+            <input type="text" name="condicion" placeholder="Ingrese la condición">
+
+            <label>Médico responsable *</label>
+            <input type="text" name="medico" placeholder="Nombre del médico">
+
+            <label>Hora de atención *</label>
+            <input type="time" name="hora">
+
+            <label>Horario de visitas *</label>
+            <input type="text" name="visitas" placeholder="Ej: 1:00 PM - 4:00 PM">
+
+            <button type="submit">
+                Registrar Paciente
+            </button>
+
+        </form>
     </div>
 
-    <form class="formulario" method="POST">
-
-    <label>ID de paciente</label>
-    <input type="text" name="id_paciente" placeholder="Ingrese el ID">
-
-    <label>Nombre completo del paciente</label>
-    <input type="text" name="nombre" placeholder="Ingrese el nombre completo">
-
-    <label>Edad actual</label>
-    <input type="text" name="edad" placeholder="Ingrese la edad">
-
-    <label>Género</label>
-    <input type="text" name="genero" placeholder="Ingrese el género">
-
-    <label>Condición del paciente</label>
-    <input type="text" name="condicion" placeholder="Ingrese la condición">
-
-    <label>Médico responsable</label>
-    <input type="text" name="medico" placeholder="Ingrese el médico responsable">
-
-    <label>Hora de atención</label>
-    <input type="text" name="hora" placeholder="Ingrese la hora">
-
-    <label>Horario de visitas</label>
-    <input type="text" name="horario" placeholder="Ingrese el horario de visitas">
-
-    <button type="submit">
-        Listo
-    </button>
-
-</form>
-
-    
+   
     <div class="circulo c1"></div>
     <div class="circulo c2"></div>
     <div class="circulo c3"></div>
     <div class="circulo c4"></div>
     <div class="circulo c5"></div>
-
-</div>
 
 </body>
 </html>
