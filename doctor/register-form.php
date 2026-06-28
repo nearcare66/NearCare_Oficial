8d055d82ce98268c6162e08c2bfc9430253949fa
sara elizabeth quintanilla valle <sara.quintanilla2027@adoc.superate.org>
2026-06-28 11:34:17 -0600

    Creacion de perfil del doctor

--- doctor/register-form.php
+++ doctor/register-form.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['id_doctor'])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
