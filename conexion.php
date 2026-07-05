005597f4dcbdc9c87c5cbf19db4b3cfc39fbf9e9
gerson048 <gerson.miranda2027@adoc.superate.org.sv>
2026-07-04 21:20:40 -0600

    estilo a audios y notas

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = new mysqli("localhost", "root", "", "familiares");
-
-if ($conexion->connect_error) {
-    die("Error de conexion: " . $conexion->connect_error);
-}
-
-$conexion->set_charset("utf8mb4");
-?>
--- php/registro2-data.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-$nombre = $_SESSION['registro_nombre'] ?? $_SESSION['usuario'] ?? 'usuario';
-$correo = $_SESSION['correo'] ?? '';
-
-?>
-
